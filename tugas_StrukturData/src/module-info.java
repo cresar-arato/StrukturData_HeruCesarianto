fa8196969bc8a1ed8ed2df0416d2240f6b42b575
Heru Cesarianto <52029960+cresar-arato@users.noreply.github.com>
2025-11-09 04:54:23 +0800

    upload

---
+++ tugas_StrukturData/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module tugasSatu_StrukturData {
+}
---
+++ tugas_StrukturData/src/tugasSatu_StrukturData/DumDum.java
@@ -0,0 +1,11 @@
+package tugasSatu_StrukturData;
+
+public class DumDum {
+
+	public static void main(String[] args) {
+		int nilai = 95;
+		System.out.println("nilai = " + nilai);
+
+	}
+
+}
